6f309d99e998054cd513ff066fa8a09dac1b0c9a
kieranmurray <kieran.murray@boipa.com>
2020-02-12 17:12:30 +0100

    update urls

--- TurnkeySDKDemoAndUnitTest/GlobalTurnkey/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace GlobalTurnkey.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
--- TurnkeySDKDemoAndUnitTest/GlobalTurnkey/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace GlobalTurnkey.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
